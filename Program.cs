47ee3f83e5421b0ee964ae98f4298bf49b24a0b7
pgj1 <pgj1@users.noreply.github.com>
2020-07-30 10:52:44 -0500

    Added descriptive comments.

--- Program.cs
+++ Program.cs
@@ -9,15 +9,12 @@ namespace Lab4_6_Array_To_Do_List
         {
             Console.WriteLine("Lab 4.6 To-Do List");
 
+            // Define arrays to hold days of week and tasks
             string[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
             string[] tasks = new string[7];
 
-            //for (int i = 0; i < days.Length; i++) 
-            //{
-            //    Console.WriteLine(days[i]);
-
-            //}
-
+           
+            // Capture user input into tasks array 
             Console.Write("\n Type in your Sunday task here: ");
             tasks[0] = Console.ReadLine();
 
@@ -39,7 +36,7 @@ namespace Lab4_6_Array_To_Do_List
             Console.Write(" Type in your Saturday task here: ");
             tasks[6] = Console.ReadLine();
 
-
+            // print date and task to console
             for (int i = 0; i < tasks.Length; i++)
             {
                 Console.WriteLine("\n Task for " + days[i] + ": "+ tasks[i]);
