474cc2abfe7a25f1d220c2a23da2fcad2e09d6b8
pgj1 <pgj1@users.noreply.github.com>
2020-07-30 10:31:19 -0500

    Add project files.

---
+++ Lab4_6_README.txt
@@ -0,0 +1,24 @@
+﻿LAB 4.6: Array To Do List
+Lesson Time: 60 Minutes
+
+In this lab, you’ll be working with arrays and for loops to get practice on working with collections.
+
+Scenario:
+You work for a company that runs a To-Do list web app. The website allows users to type in tasks 
+for each day of the week so that they remember to do them. You’ve been asked to use C# to create 
+a small to-do list planner. As usual, someone else will handle how the code will work on the web, 
+you’ve been asked to write the initial test version of the logic.
+
+Requirements:
+The do-to list planner with cycle through each day of the week, pausing to let the user type in a task. 
+The planner will save each task the user types in, and then display all of the days and tasks together. 
+See the screenshot below for an example of the screen output.
+
+Tips:
+* You will keep each day in an array of “days”.
+* You’ll save the tasks in an array of “tasks”.
+* You need to setup for each loops to loop through the days and tasks.
+
+To view our solution, view the 4_6LabSolution on our GitHub Page.
+https://github.com/ACTWebDev/SD104/blob/master/4_6LabSolution/4_6LabSolution/Program.cs
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab4_6_Array_To_Do_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4.6 To-Do List");
+
+            string[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+            string[] tasks = new string[7];
+
+            //for (int i = 0; i < days.Length; i++) 
+            //{
+            //    Console.WriteLine(days[i]);
+
+            //}
+
+            Console.Write("\n Type in your Sunday task here: ");
+            tasks[0] = Console.ReadLine();
+
+            Console.Write(" Type in your Monday task here: ");
+            tasks[1] = Console.ReadLine();
+
+            Console.Write(" Type in your Tuesday task here: ");
+            tasks[2] = Console.ReadLine();
+
+            Console.Write(" Type in your Wednesday task here: ");
+            tasks[3] = Console.ReadLine();
+
+            Console.Write(" Type in your Thursday task here: ");
+            tasks[4] = Console.ReadLine();
+
+            Console.Write(" Type in your Friday task here: ");
+            tasks[5] = Console.ReadLine();
+
+            Console.Write(" Type in your Saturday task here: ");
+            tasks[6] = Console.ReadLine();
+
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Console.WriteLine("\n Task for " + days[i] + ": "+ tasks[i]);
+               // Console.WriteLine(tasks[i]);
+
+            }
+
+
+
+        }
+    }
+}
